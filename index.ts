82c79e9ebe1178d5faec5be30ee3031bcd787a44
dg12988 <doug.goldberg@me.com>
2021-08-10 13:04:51 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
+const failingHttpRequest$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error(new Error('Timeout'));
+  }, 3000);
+});
 
+console.log('App Started');
+
+failingHttpRequest$.pipe(
+  catchError(error => of('Fallback value'))
+).subscribe(
+  value => console.log(value)
+);
